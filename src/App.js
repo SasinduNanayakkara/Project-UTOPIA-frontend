02830d3e4ce6b63b7a79a8bc53acadc8cf3aee03
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-03-07 03:04:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/login/  ",
+      element: <Login />
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
