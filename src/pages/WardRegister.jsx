d0a2a03fe739567dc452402ca0245ab566945469
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-03-10 02:35:09 +0530

    Implement ward register

---
+++ src/pages/WardRegister.jsx
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+import { baseUrl } from '../App';
+import Header from '../Components/Header'
+
+function WardRegister() {
+  const location = useLocation();
+  const [name, setName] = useState("");
+  const [hospitalID, setHospitalID] = useState("");
+  const [no_of_beds, setNo_of_beds] = useState("");
+
+  const handleSubmit = async (e) => {
+    setHospitalID(location.state.hospitalId);
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${baseUrl}/ward`, { name, hospitalID, no_of_beds});
+      if (response) {
+        console.log(response);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <section className="h-screen">
+      <Header />
+      <div className="h-full">
+        <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+          <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+            <img src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="w-full" alt="Sample image" />
+          </div>
+          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+            <form class="w-full max-w-lg">
+              <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                  <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    Ward Name
+                  </label>
+                  <input onChange={(e) => setName(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Ward Name" />
+                </div>
+              </div>
+              <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                  <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    No of Beds
+                  </label>
+                  <input onChange={(e) => setNo_of_beds(e.target.value)} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="No Of Beds" />
+                </div>
+              </div>
+              <div class="flex items-center justify-between mb-6">
+                <button onClick={(e) => { handleSubmit(e) }} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default WardRegister
+
