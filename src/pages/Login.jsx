a298527bfc66c467179f5a56b57047b39b0155bd
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-03-12 03:15:55 +0530

    Update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const onsubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${baseUrl}/login`, {username, password});
+      const response = await axios.post(`${baseUrl}/login`, { username, password });
       if (response) {
         localStorage.setItem("token", response.data.token);
         localStorage.setItem("username", response.data.data.username);
@@ -34,18 +34,18 @@ const Login = () => {
         else if (response.data.data.role === "doctor") {
           navigate("/doctorDashboard");
         }
-        else if(response.data.data.role === "nurse") {
+        else if (response.data.data.role === "nurse") {
           navigate("/nurseDashboard");
         }
       }
     }
-    catch(err) {
+    catch (err) {
       console.log(err);
     }
   }
   return (
     <section className="h-screen">
-        <Header/>
+      <Header />
       <div className="h-full">
         <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
           <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
@@ -54,7 +54,7 @@ const Login = () => {
           <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
             <form className='mr-16'>
               <div className="relative mb-6" data-te-input-wrapper-init>
-                <input onChange={(e) => setUsername(e.target.value)} type="text" className=" peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-black text-black dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"  id="exampleFormControlInput2" placeholder="Username" />
+                <input onChange={(e) => setUsername(e.target.value)} type="text" className=" peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-black text-black dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="exampleFormControlInput2" placeholder="Username" />
                 <label htmlFor="exampleFormControlInput2" className="pointer-events-none focus:text-sky-500 absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-gray-400 dark:peer-focus:text-blue-600">Username
                 </label>
               </div>
@@ -63,12 +63,12 @@ const Login = () => {
                 <label htmlFor="exampleFormControlInput22" className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-gray-400 dark:peer-focus:text-blue-600">Password
                 </label>
               </div>
-              
+
               <div className="text-center lg:text-left">
                 <button type="button" onClick={(e) => onsubmit(e)} className="inline-block rounded bg-blue-600 px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]" data-te-ripple-init data-te-ripple-color="light">
                   Login
                 </button>
-                
+
               </div>
             </form>
           </div>
