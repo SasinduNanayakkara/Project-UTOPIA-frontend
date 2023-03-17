5770d4c16f8ea8ec9e7eeefee2486d33c83d02a7
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2023-03-17 18:32:31 +0530

    style nurse and patients

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -82,10 +82,11 @@ function Patients() {
   const navigateToPatientUpdate = (event, rowData) => {
     navigate('/patientUpdate', { state: { wardID: wardID, patientID: rowData._id, HospitalID: HospitalID } })
   }
-  const PatientTitle = <span className='font-bold'>Hospital Name</span>
+  const PatientTitle = <span className='font-bold'>Patient Name</span>
   const status = <span className='font-bold'>Status</span>
   const AdmitDate = <span className='font-bold'>Admit Date</span>
   const Address = <span className='font-bold'>Address</span>
+  const NIC = <span className='font-bold'>NIC</span>
   const PhoneNumber = <span className='font-bold'>Phone Number</span>
 
   return (
@@ -112,6 +113,7 @@ function Patients() {
             { title: status, field: 'status' },
             { title: AdmitDate, field: 'admit_date' },
             { title: Address, field: 'address' },
+            { title: NIC, field: 'NIC' },
             { title: PhoneNumber, field: 'phone' },
             
 
