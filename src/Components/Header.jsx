2174c166b678b11def400a08ab2da94920298de3
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-03-18 20:36:30 +0530

    login bug fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -22,6 +22,8 @@ const Header = ({first_name, username}) => {
 
     useEffect(() => {
         setNameText(localStorage.getItem("first_name") || localStorage.getItem("name"));
+        console.log("name", nameText);
+        console.log("loggedIn", localStorage.getItem("loggedIn"));
     });
 
   return (
@@ -33,7 +35,7 @@ const Header = ({first_name, username}) => {
             </a>
             <div class="flex items-center lg:order-2">
                 <a href="#" onClick={(e) => handleLogin(e)} class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-gray-800">{localStorage.getItem("loggedIn") ? "Logout" : "Log in"}</a>
-                <a class="text-gray-800 dark:text-white  focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2  focus:outline-none dark:focus:ring-gray-800">{nameText}</a>
+                <a class="text-gray-800 dark:text-white  focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2  focus:outline-none dark:focus:ring-gray-800">{localStorage.getItem("loggedIn") ? nameText : ""}</a>
                 
                 <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
